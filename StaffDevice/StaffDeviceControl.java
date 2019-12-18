bb736bb7df818a25de964d58e09be0c5459d2d32
金山　晃 <@DESKTOP-TJCGFOD>
2019-12-18 21:47:26 +0900

    test

--- StaffDevice/StaffDeviceControl.java
+++ StaffDevice/StaffDeviceControl.java
@@ -2,4 +2,50 @@ package パッケージ0.StaffDevice;
 
 public class StaffDeviceControl {
 
+	public static void main(String[] args) {
+
+		StaffDeviceGUI sdg = new StaffDeviceGUI();
+		ICCardReader icr = new ICCardReader();
+		DatabaseControl dbc = new DatabaseControl();
+		Scanner scan = new Scanner(System.in);
+
+		int command;
+		String correctiondata;
+		String id = null;
+		boolean hasID;
+
+		while(true) {
+			while(id == null) {
+				System.out.println("ICカードをタッチしてください");
+				id = icr.readIC();
+				if(id == null) {
+					sdg.displayReadingError();
+				}
+			}
+
+			hasID = dbc.searchID(id);
+
+			if(hasID = false)sdg.displayRegistrationError();
+			else {
+				System.out.println("以下から捜査を選択してください．");
+				System.out.println("1:エラーを解消");
+				System.out.println("2:IDを削除");
+
+				command = scan.nextInt();
+
+				if(command == 1) {
+					sdg.displayStateError();
+					correctiondata = sdg.requestCorrectingData();
+					dbc.correctRidingStat();
+					System.out.println("ICカードのデータを書き換えました．\n");
+				}
+				else if(command == 2) {
+					dbc.deleteID();
+					sdg.displayDeleteID();
+				}
+				else System.out.println("最初から操作してください\n");
+			}
+		}
+	}
+
 }
--- StaffDevice/StaffDeviceGUI.java
+++ StaffDevice/StaffDeviceGUI.java
@@ -2,26 +2,27 @@ package パッケージ0.StaffDevice;
 
 import パッケージ0.String;
 
+import java.util.Scanner;
+
 public class StaffDeviceGUI {
+	Scanner scan = new Scanner(System.in);
 
 	public void displayReadingError() {
-
+		System.out.println("ICカードの読み込みに失敗しました．");
 	}
-
 	public void displayRegistrationError() {
-
+		System.out.println("ICカードが登録されていません．");
 	}
-
-	public void displayDeleteID() {
-
-	}
-
-	public String requestCorrectingData() {
-		return null;
-	}
-
 	public void displayStateError() {
-
+		System.out.println("ICカードの乗降データが不正です．");
 	}
-
+	public String requestCorrectingData() {
+		System.out.println("直前の乗降駅を入力してください．");
+		String correctingdata = scan.next();
+		return correctingdata;
+	}
+	public void displayDeleteID() {
+		System.out.println("IDを削除しました．\n1");
+	}
+}
 }
